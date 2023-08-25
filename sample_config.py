fda3353a862f70ba8217ff98ff61b6065648cdb2
MalikGit <97935254+Virtualunionsex@users.noreply.github.com>
2023-08-26 01:13:15 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -13,15 +13,15 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 class Config(object):
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6278243144:AAFugrKWisjrTXTrbnX3e7c4W9wVxjASptY")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6697713258:AAFDfdivj4INPJVDlfI_k7APbYxds4wmGbs")
     # Get from my.telegram.org
-    APP_ID = int(os.environ.get("APP_ID", "16246834"))
+    APP_ID = int(os.environ.get("APP_ID", "22825629"))
     # Get from my.telegram.org
-    API_HASH = os.environ.get("API_HASH", "29b3ffa9245c07f05375b92f38e8f13d")
+    API_HASH = os.environ.get("API_HASH", "e8db542482a1638b4e5b03ed1ddae521")
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "postgresql://pfewsazv:NFJuP00vvb9dup3vS8jzp0ZpNYusWXtS@raja.db.elephantsql.com/pfewsazv")
+    DB_URI = os.environ.get("DATABASE_URL", "postgres://jncqeegk:vp28D-Z-omAp8V7uh5bi4B5LlsYlL3nn@tyke.db.elephantsql.com/jncqeegk")
     # List of admin user ids for special functions(
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1715348447").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6404342282").split())
 
 
 def LOGGER(name: str) -> logging.Logger:
